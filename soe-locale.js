97c5c23f7e1d4c5ee843a6572c2462ce4f250a70
atemu <atemulp@gmail.com>
2018-02-17 22:00:44 +0100

    Fixed object extraction from the strings element, see https://stackoverflow.com/questions/41255430/get-nth-element-of-a-key-in-an-object

--- soe-locale.js
+++ soe-locale.js
@@ -67,8 +67,9 @@ function write(strings, dataFile, indexFile, callback) {
 
     indexStream.write("## Count:   " + strings.length + "\r\n");
 
-    for (var i=0;i<Object.keys(strings).length;i++) {
-        string = strings[i];
+    //for (var i=0;i<Object.keys(strings).length;i++) {
+    for (var i=0;i<100;i++) { //debug loop
+        string = strings[Object.keys(strings)[i]];
 
         dataLength = (string.hash+"").length + string.flags.length + string.string.length + 2;
 
